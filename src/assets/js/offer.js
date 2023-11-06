f874f843075ca22e68718001e8f1faec31eaab5a
GoldenDragon0705 <GoldenDragon0705@outlook.com>
2023-11-06 13:24:46 -0500

    update offer container

--- src/assets/js/offer.js
+++ src/assets/js/offer.js
@@ -13,28 +13,38 @@ Offer.prototype.init = function() {
                               <a class="nav-link" data-bs-toggle="tab" href="#' + self.id + '" data-offername="' + self.offername + '">' + self.offername + ' *</a>\
                             </li>');
   offersContainer.append('<div class="tab-pane container-fluid px-0 py-2" id="' + self.id + '" role="tabpanel">\
+                              <div class="offer-actions-bar mt-2 mb-3 p-3 d-flex border rounded">\
+                                <a href="" class="me-4"><i class="fa fa-save"></i> Save this offer</a>\
+                                <a href="" class="me-4"><i class="fa fa-file-word-o"></i> Generate docx</a>\
+                                <a href="" class="me-auto"><i class="fa fa-file-pdf-o"></i> Generate pdf</a>\
+                                <a href="" class="me-4"><i class="fa fa-cog"></i> Setting</a>\
+                                <a href="" >Close</a>\
+                              </div>\
                             <div class="row">\
-                            <div class="col-md-3">\
-                              <div class="w3-card w3-indigo w3-round-large px-2 py-2">\
-                                <h2 class="text-center w3-border-bottom pb-2">Existing brands</h2>\
-                                <div class="d-grid mb-2">\
-                                  <div class="form-group mb-2">\
-                                    <input type="text" class="form-control new-brand-name" placeholder="New brand name">\
-                                  </div>\
-                                  <button class="w3-btn w3-ripple w3-blue w3-round-large btn-block btn-create-new-brand">Create new brand</button>\
-                                </div> \
-                                <div>\
-                                  <ul class="list-group list-group-flush bg-transparent brands-container">\
-                                  </ul>\
-                                </div> \
+                              <div class="col-md-3">\
+                                <div class="w3-card w3-indigo w3-round-large px-2 py-2">\
+                                  <h2 class="text-center w3-border-bottom pb-2">Existing brands</h2>\
+                                  <div class="d-grid mb-2">\
+                                    <div class="form-group mb-2">\
+                                      <input type="text" class="form-control new-brand-name" placeholder="New brand name">\
+                                    </div>\
+                                    <button class="w3-btn w3-ripple w3-blue w3-round-large btn-block btn-create-new-brand">Create new brand</button>\
+                                  </div> \
+                                  <div>\
+                                    <ul class="list-group list-group-flush bg-transparent brands-container">\
+                                    </ul>\
+                                  </div> \
+                                </div>\
+                                <div class="d-flex justify-content-center my-2 ">\
+                                  <a href="javascript: class="btn-delete-all-brands">Delete all brands</a>\
+                                </div>\
                               </div>\
-                            </div>\
-                            <div class="col-md-9 items-container">\
-                              <div class="w3-panel w3-dark-gray my-0 no-brand-alert">\
-                                <h3>No brands.</h3>\
-                                <p>Please create a new brand.</p>\
+                              <div class="col-md-9 items-container">\
+                                <div class="w3-panel w3-dark-gray my-0 no-brand-alert">\
+                                  <h3>No brands.</h3>\
+                                  <p>Please create a new brand.</p>\
+                                </div>\
                               </div>\
-                            </div>\
                           </div>\
                         </div>');
 
@@ -98,6 +108,7 @@ Offer.prototype.createBrand = function(brandName) {
                                                         <a href="javascript:" class="me-2">Import new images</a>\
                                                         <a href="javascript:" class="me-2">Delete all items</a>\
                                                         <a href="javascript:" class="me-2">Delete this brand</a>\
+                                                        <a href="javascript:" class="me-2">Change brand name</a>\
                                                       </div>\
                                                       <div>\
                                                         <button class="w3-btn w3-ripple w3-teal w3-round-large btn-create-item" data-bs-toggle="modal" data-bs-target="#create-new-item">Create new item</button>\
