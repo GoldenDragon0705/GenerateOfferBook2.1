4343baecb890430cb9ff260f9b0d4a338cd4358c
Light Forward <robertperry19981010@gmail.com>
2023-11-08 04:48:00 -0800

    the image browse editing has completed

--- preload.js
+++ preload.js
@@ -2,20 +2,29 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
     openDialog: (method, config) => {
-        const result = ipcRenderer.invoke('dialog', method, config);
-        return result;
+      const result = ipcRenderer.invoke('dialog', method, config);
+      return result;
+    },
+    changeDialog: (method, config) => {
+      const result = ipcRenderer.invoke('change_Dialog', method, config);
+      return result;
     },
     saveFileNames: (callback) => {
         ipcRenderer.on("file_names", (event, data) => {
             callback(data);
         })
     },
-  loadImages : (offerId, brandId, mode) => {
-    ipcRenderer.invoke('load_images', {offerId, brandId, mode});
-  },
-  onFilenames: (callback) => {
-    ipcRenderer.on('loaded_filenames', (e, params) => {
-      callback(params);
+    addFileNames: (callback) => {
+      ipcRenderer.on("add_images", (event, data) => {
+        callback(data);
+      })
+    },
+    loadImages : (offerId, brandId, mode) => {
+      ipcRenderer.invoke('load_images', {offerId, brandId, mode});
+    },
+    onFilenames: (callback) => {
+      ipcRenderer.on('loaded_filenames', (e, params) => {
+        callback(params);
     });
   }
 });
